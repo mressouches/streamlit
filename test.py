57f114697e9243b79d97d93f1dc28398fb916b74
yotoshi <boris.tang@outlook.fr>
2023-11-20 11:13:55 +0100

    Update test.py

--- test.py
+++ test.py
@@ -7,37 +7,37 @@ def convert_df(df):
     return df.to_csv(index=False,encoding='utf-8').encode('utf-8')
 tab1, tab2, tab3 = st.tabs(["SOl", "[2023] Advent calendar", "[STARS]Insurance"])
 with tab1:
-    uploaded_file=tab1.file_uploader('Choose a file')
+    uploaded_file=st.file_uploader('Choose a file')
     try:
         df=pd.read_excel(uploaded_file, sheet_name='Matrix')
     except Exception as e:
         st.write(e)
 
     df.columns=df.columns.str.strip()
-    tab1.write(df)
+    st.write(df)
     csv = convert_df(df)
 
-    tab1.download_button(
+    st.download_button(
         label="Download data as CSV",
         data=csv,
         file_name='large_df.csv',
         mime='text/csv',
     )
 
-with tab2:
-    uploaded_advent_file=tab2.file_uploader('Choose a file')
+with st:
+    uploaded_advent_file=st.file_uploader('Choose a file')
     try:
         df=pd.read_excel(uploaded_advent_file, sheet_name='Campaigns',skiprows=3,skipfooter=5)
     except Exception as e:
         st.write(e)
 
     df.columns=df.columns.str.strip()
-    tab2.write(df)
+    st.write(df)
 
 
     csv = convert_df(df)
 
-    tab2.download_button(
+    st.download_button(
         label="Download data as CSV",
         data=csv,
         file_name='large_df.csv',
