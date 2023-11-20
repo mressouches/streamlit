dfa35ffdab367dc79d23105ce78a64b164ab19b1
yotoshi <boris.tang@outlook.fr>
2023-11-20 11:04:15 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,23 +2,22 @@ import streamlit as st
 import pandas as pd
 
 st.write("test")
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv(index=False,encoding='utf-8').encode('utf-8')
 tab1, tab2, tab3 = st.tabs(["SOl", "[2023] Advent calendar", "[STARS]Insurance"])
 with tab1:
-    uploaded_file=st.file_uploader('Choose a file')
+    uploaded_file=tab1.file_uploader('Choose a file')
     try:
         df=pd.read_excel(uploaded_file, sheet_name='Matrix')
     except Exception as e:
         st.write(e)
 
     df.columns=df.columns.str.strip()
-    st.write(df)
-    def convert_df(df):
-        # IMPORTANT: Cache the conversion to prevent computation on every rerun
-        return df.to_csv(index=False,encoding='utf-8').encode('utf-8')
-
+    tab1.write(df)
     csv = convert_df(df)
 
-    st.download_button(
+    tab1.download_button(
         label="Download data as CSV",
         data=csv,
         file_name='large_df.csv',
@@ -26,21 +25,19 @@ with tab1:
     )
 
 with tab2:
-    uploaded_advent_file=st.file_uploader('Choose a file')
+    uploaded_file=tab2.file_uploader('Choose a file')
     try:
-        df=pd.read_excel(uploaded_advent_file, sheet_name='Campaigns',skiprows=3,skipfooter=5)
+        df=pd.read_excel(uploaded_file, sheet_name='Campaigns',skiprows=3,skipfooter=5)
     except Exception as e:
         st.write(e)
 
     df.columns=df.columns.str.strip()
-    st.write(df)
-    def convert_df(df):
-        # IMPORTANT: Cache the conversion to prevent computation on every rerun
-        return df.to_csv(index=False,encoding='utf-8').encode('utf-8')
+    tab2.write(df)
+
 
     csv = convert_df(df)
 
-    st.download_button(
+    tab2.download_button(
         label="Download data as CSV",
         data=csv,
         file_name='large_df.csv',
