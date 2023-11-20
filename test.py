60b60e7e47923f790cd05aced23c577ad78b8301
yotoshi <boris.tang@outlook.fr>
2023-11-20 11:19:30 +0100

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def convert_df(df):
     return df.to_csv(index=False,encoding='utf-8').encode('utf-8')
 tab1, tab2, tab3 = st.tabs(["SOl", "[2023] Advent calendar", "[STARS]Insurance"])
 with tab1:
-    uploaded_file=st.file_uploader('Choose a file')
+    uploaded_file=st.file_uploader('Choose a file',key='sol')
     try:
         df=pd.read_excel(uploaded_file, sheet_name='Matrix')
     except Exception as e:
@@ -25,7 +25,7 @@ with tab1:
     )
 
 with st:
-    uploaded_advent_file=st.file_uploader('Choose a file')
+    uploaded_advent_file=st.file_uploader('Choose a file',key='advent_calendar')
     try:
         df=pd.read_excel(uploaded_advent_file, sheet_name='Campaigns',skiprows=3,skipfooter=5)
     except Exception as e:
